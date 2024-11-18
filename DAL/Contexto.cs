ddd52ed65e3c28f1f72f9f09cc9bde2a7e9d4ec3
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-11-18 16:24:54 -0400

    añadiendo el contexto

---
+++ DAL/Contexto.cs
@@ -0,0 +1,14 @@
+﻿using FrannielArias_AP1_P2.Models;
+using FrannielArias_AP1_P2.Services;
+using Microsoft.EntityFrameworkCore;
+
+namespace FrannielArias_AP1_P2.DAL
+{
+    public class Contexto : DbContext
+    {
+        public Contexto(DbContextOptions<Contexto> options)
+           : base(options) { }
+
+        public DbSet<Registro> Registro { get; set; }
+    }
+}
