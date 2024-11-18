795a673eba0d5dadc707bb33e9a1a95ffa2fcd29
FrannielArias <156683746+FrannielArias@users.noreply.github.com>
2024-11-18 16:25:28 -0400

    Cree la plantilla del modelo

---
+++ Models/Registro.cs
@@ -0,0 +1,10 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace FrannielArias_AP1_P2.Models
+{
+    public class Registro
+    {
+        [Key]
+        public int RegitroId {  get; set; }
+    }
+}
